8d74f40dbe8604a1fbcc4e905cdde89096e2d1b3
eracle <eracle@posteo.eu>
2026-02-19 10:46:59 +0000

    Fix pending detection in connection_status and standardize action __main__ blocks with argparse

--- manage.py
+++ manage.py
@@ -20,7 +20,7 @@ from linkedin.management.setup_crm import setup_crm
 
 logging.getLogger().handlers.clear()
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(message)s",
 )
 
